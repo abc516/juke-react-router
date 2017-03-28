ccd37ff079621634029a1ce91ad1311438886c7e
Anton Cataldi <antoncataldi@gmail.com>
2017-03-28 18:32:41 -0400

    single album route working

--- browser/react/index.js
+++ browser/react/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import AppContainer from './containers/AppContainer';
-import {Router, Route, hashHistory} from 'react-router'
+import {Router, Route, IndexRedirect, hashHistory} from 'react-router'
 import Albums from './components/Albums'
-
+import Album from './components/Album'
 
 ReactDOM.render(
   <Router history={hashHistory} >
-	  <Route path='/' component={AppContainer}>
-	  	<Route path='/albums' component={Albums}></Route>
+	  <Route path='/' component={AppContainer} >
+      <IndexRedirect to="/albums" />
+	    <Route path='/albums' component={Albums}></Route>
+      <Route path="albums/:albumId" component={Album} />
 	  </Route>
   </Router>,
 
